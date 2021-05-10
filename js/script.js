3f4cceb9d4fbd63b9577b3bcc60bd9a3151e74c8
BeatrizBacelar <beabacelar18@gmail.com>
2021-05-09 21:37:04 -0300

    feat: Adição da função para cadastrar novas tarefas.

--- js/script.js
+++ js/script.js
@@ -17,6 +17,9 @@ let tarefas = [
 ];
 
 function renderizarTarefas(){
+    // Limpar a listagem de items antes de renderizar novamente a tela
+    lista.innerHTML = '';
+
     for(tarefa of tarefas){
         // Criar o item da lista
         let itemLista = document.createElement('li');
@@ -37,6 +40,39 @@ function renderizarTarefas(){
 
 // Executando a função para renderizar as tarefas
 renderizarTarefas();
+
+// 1 -Precisamos "escutar" o vendo de click no botão
+btn.onclick = function(){
+// 2 - Precisamos capturar o valor digitado pelo usuário no input
+let novaTarefa = input.value;
+
+if (novaTarefa != ''){
+    // 3 - Precisamos atualizar a nova tarefa na listar (array) de tarefas e renderizar a tela
+    tarefas.push(novaTarefa);
+
+    // Executando a função para renderizar as tarefas
+    renderizarTarefas();
+
+    // Limpar o input
+    input.value = '';
+
+   
+
+}else{
+    let card = document.querySelector('.card');
+
+    let span = document.createElement('span');
+    span.setAttribute('class', 'alert alert-warning');
+    
+    let msg = document.createTextNode('Você precisa informar  a tarefa!!');
+
+    span.appendChild(msg);
+    card.appendChild(span);
+    }
+
+}
+
+
 
 
 
